b975e46f11e6fb8c4829e5436cd3a31247655c99
gerald <gerald@1a95236e-1df7-11df-b8d3-59fc97e42aa7>
2010-02-02 15:09:30 +0000

    initial import

---
+++ project/src/org/hystudio/dosbox/DosBoxGLSurfaceView.java
@@ -0,0 +1,13 @@
+package org.hystudio.dosbox;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class DosBoxGLSurfaceView extends GLSurfaceView {
+
+	public DosBoxGLSurfaceView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
