4c23defe9ac9545c5375757becfda54b85e77020
Jenvor <Jenvor@1a95236e-1df7-11df-b8d3-59fc97e42aa7>
2010-02-22 22:43:41 +0000

    Code that is used to built aDOSBOX WVGA 0.1Alpha.

---
+++ tags/aDOSBox-WVGA-0.1Alpha/project/src/org/hystudio/dosbox/DosBoxMain.java
@@ -0,0 +1,155 @@
+package org.hystudio.dosbox;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class DosBoxMain extends Activity {
+	public static final String ApplicationName = "dosbox";
+	private DemoGLSurfaceView mGLView;
+	private PowerManager.WakeLock wakeLock;
+
+	static {
+		System.loadLibrary(ApplicationName);
+	}
+
+	/** Called when the activity is first created. */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		mGLView = new DemoGLSurfaceView(this);
+		setContentView(mGLView);
+		// Receive keyboard events
+		mGLView.setFocusableInTouchMode(true);
+		mGLView.setFocusable(true);
+		mGLView.requestFocus();
+		PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		wakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK,
+				ApplicationName);
+		wakeLock.acquire();
+	}
+
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+	}
+}
+
+class DemoRenderer implements GLSurfaceView.Renderer {
+
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		nativeInit();
+	}
+
+	public void onSurfaceChanged(GL10 gl, int w, int h) {
+		// gl.glViewport(0, 0, w, h);
+		nativeResize(w, h);
+	}
+
+	public void onDrawFrame(GL10 gl) {
+		nativeRender();
+	}
+
+	public void exitApp() {
+		nativeDone();
+	};
+
+	private static native void nativeInit();
+
+	private static native void nativeResize(int w, int h);
+
+	private static native void nativeRender();
+
+	private static native void nativeDone();
+
+}
+
+class DemoGLSurfaceView extends GLSurfaceView {
+	public DemoGLSurfaceView(Activity context) {
+		super(context);
+
+		// Copy the configuration file from sdcard to
+		// /data/data/org.hystudio.dosbox/files/
+		File srcFile = new File("/sdcard/dosbox.conf");
+		File destFile = new File(
+				"/data/data/org.hystudio.dosbox/files/dosbox.conf");
+		if (srcFile.exists()) {
+			try {
+				InputStream in = new FileInputStream(srcFile);
+				OutputStream out = new FileOutputStream(destFile);
+				byte[] buf = new byte[1024];
+				int len;
+				while ((len = in.read(buf)) > 0) {
+					out.write(buf, 0, len);
+				}
+				in.close();
+				out.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		mParent = context;
+		mRenderer = new DemoRenderer();
+		setRenderer(mRenderer);
+	}
+
+	@Override
+	public boolean onTouchEvent(final MotionEvent event) {
+		// TODO: add multitouch support (added in Android 2.0 SDK)
+		int action = -1;
+		if (event.getAction() == MotionEvent.ACTION_DOWN)
+			action = 0;
+		if (event.getAction() == MotionEvent.ACTION_UP)
+			action = 1;
+		if (event.getAction() == MotionEvent.ACTION_MOVE)
+			action = 2;
+		if (action >= 0) {
+			nativeMouse((int) event.getX(), (int) event.getY(), action);
+		}
+		return true;
+	}
+
+	public void exitApp() {
+		mRenderer.exitApp();
+	};
+
+	@Override
+	public boolean onKeyDown(int keyCode, final KeyEvent event) {
+		nativeKey(keyCode, 1);
+		return true;
+	}
+
+	@Override
+	public boolean onKeyUp(int keyCode, final KeyEvent event) {
+		nativeKey(keyCode, 0);
+		return true;
+	}
+
+	DemoRenderer mRenderer;
+	Activity mParent;
+
+	public static native void nativeMouse(int x, int y, int action);
+
+	public static native void nativeKey(int keyCode, int down);
+}
